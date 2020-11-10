9f610ade3e273067bfd94711d0bcec8b90fdd56e
ArslanInfinite <ali.arslan319@gmail.com>
2020-11-09 20:50:56 -0500

    change helper methods in application_controller to better suit users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,19 @@ class ApplicationController < ActionController::Base
   before_action :authorized
   helper_method :current_user, :logged_in?
 
+  private 
+
   def current_user
-    User.find_by(id: session[:user_id])
-  end
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+   end
 
   def logged_in?
     !!current_user
   end
 
-  def authorized
-    redirect_to login_path unless logged_in?
-  end
+  def redirect_if_not_logged_in
+      redirect_to '/' if !logged_in?
+   end
 
   def activity
     session[:activity] ||= []
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :activities
+  resources :reservations
+  
+  get '/users/:id/activities', to: 'users#user_activities', as: 'user_activities'
+
+  get "/welcome", to: "users#welcome", as: "welcome"
+
+  get '/mainpage', to: 'sessions#mainpage', as: 'mainpage'
+
+  get '/login', to: 'sessions#new', as: 'login'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/add_funds', to: 'users#add_funds', as: 'add_funds'
+
+  get '/reviews/new/:activity_id', to: 'reviews#new', as: 'new_review'
+  resources :reviews, only: [:create]
 end
