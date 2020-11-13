22761d2d3066d28188bc18174adf704420f707ba
ArslanInfinite <ali.arslan319@gmail.com>
2020-11-12 22:57:01 -0500

    add standard methods to reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,28 @@
 class ReviewsController < ApplicationController
 
+  def index
+    @reviews = Review.all
+  end
 
-  
+  def show
+    @reviews = Review.all.select {|review| review.activity.id = params[:id]}
+  end
+
+  def new
+    @activity = Activity.find(params[:activity_id])
+    @review = Review.new
+  end
+
+  def create
+    Review.create(review_params)
+    redirect_to mainpage_path
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :user_id, :activity_id)
+  end
+
+
 end
