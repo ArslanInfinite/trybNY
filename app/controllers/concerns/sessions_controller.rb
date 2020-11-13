21f2e38661f27c1304f23b3095c6dccec0ef5ce6
ArslanInfinite <ali.arslan319@gmail.com>
2020-11-12 23:48:11 -0500

    add methods to sessions_controller.rb

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create, :destroy]
 
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to mainpage_path
+    else
+      flash[:notice] = 'Invalid username or password.'
+      redirect_to login_path
+    end
+  end
 
 
 end
