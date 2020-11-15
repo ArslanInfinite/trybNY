64abbc506727d096bc3f074f9b721e22b58d1eb1
ArslanInfinite <ali.arslan319@gmail.com>
2020-11-15 17:35:08 -0500

    add user_id to activities migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 2020_11_15_223316) do
 
   create_table "activities", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 5) do
     t.integer "capacity"
     t.integer "price"
     t.datetime "datetime"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 5) do
     t.boolean "admin", default: false, null: false
   end
 
+  add_foreign_key "activities", "users"
 end
