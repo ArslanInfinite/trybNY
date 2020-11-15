c2dc131df292170de95d6f8fe6d4a393aae908f7
ArslanInfinite <ali.arslan319@gmail.com>
2020-11-15 17:22:14 -0500

    change Like model and fix validations for User

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ActiveRecord::Base
 
   belongs_to :user, polymorphic: true
-  belongs_to :activity, polymorphic: true
+  belongs_to :review, polymorphic: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,19 @@ class User < ActiveRecord::Base
 
   has_secure_password 
 
-  has_many :reservations
-  has_many :activities 
-  has_many :activities, through: :reservations
-  has_many :reviews, through: :activities 
-  has_many :likes, as: :likable
+  has_many :reservations, dependent: :destroy
+  has_many :activities, dependent: :destroy 
+  has_many :activities, through: :reservations, dependent: :destroy
+  has_many :reviews, through: :activities, dependent: :destroy
+  has_many :likes, as: :likable, dependent: :destroy
 
   validates :username, presence: true 
   validates :username, uniqueness: true
   validates :username, length: { minimum: 4, too_short: "%{count} characters is the minimum allowed" }
   validates :username, length: { maximum: 20, too_long: "%{count} characters is the maximum allowed" }
 
-  validates :password, presence: true #{ case_sensitive: false }
-  validates :password, confirmation: true #{ case_sensitive: false }
-  validates :password_confirmation, presence: true #{ case_sensitive: false }
+  validates :password, presence: true, case_sensitive: false
+  validates :password, confirmation: true, case_sensitive: false 
+  validates :password_confirmation, presence: true, case_sensitive: false 
 
 end
